725c62820afc559dc428f3022a58941cbf9906ee
Carlos Meza <carlos@digitalr00ts.com>
2019-10-01 16:06:37 -0700

    added loglevels

--- src/c7n_broom/__init__.py
+++ src/c7n_broom/__init__.py
@@ -5,12 +5,18 @@ import pkg_resources
 
 from .base import REGION_MAP, C7nConfig
 
-
 try:
     __version__ = pkg_resources.get_distribution(__name__).version
 except pkg_resources.DistributionNotFound:
     __version__ = ""
 
-logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __all__ = ["C7nConfig"]
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+logging.getLogger(__name__).setLevel(logging.DEBUG)
+logging.getLogger("custodian").setLevel(logging.INFO)
+logging.getLogger("botocore").setLevel(logging.ERROR)
+logging.getLogger("urllib3").setLevel(logging.ERROR)
+logging.getLogger("s3transfer").setLevel(logging.ERROR)
+logging.getLogger("urllib3").setLevel(logging.ERROR)
