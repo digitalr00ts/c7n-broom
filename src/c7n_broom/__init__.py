b24d2227e15cb50a5e9da3a0b03f773a71a1f9d5
Carlos Meza <carlos@digitalr00ts.com>
2019-11-19 11:23:48 -0800

    removed setting logger loglevel for c7n_broom

--- src/c7n_broom/__init__.py
+++ src/c7n_broom/__init__.py
@@ -17,7 +17,6 @@ except pkg_resources.DistributionNotFound:
 __all__ = []
 
 _logging.getLogger(__name__).addHandler(_logging.NullHandler())
-_logging.getLogger(__name__).setLevel(_logging.DEBUG)
 _logging.getLogger("custodian").setLevel(_logging.INFO)
 _logging.getLogger("botocore").setLevel(_logging.ERROR)
 _logging.getLogger("urllib3").setLevel(_logging.ERROR)
