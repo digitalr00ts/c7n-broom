58e9667730b0254d98737cfccac8651c45ff1d76
Carlos Meza <carlos@digitalr00ts.com>
2019-12-02 18:24:11 -0800

    fixed get_policy_data

--- src/c7n_broom/config/main.py
+++ src/c7n_broom/config/main.py
@@ -1,5 +1,6 @@
 """ Main of c7n_broom.config """
 import dataclasses
+import itertools
 import logging
 import os
 import pathlib
@@ -141,7 +142,7 @@ class C7nConfig(c7n.config.Config):  # pylint: disable=too-many-instance-attribu
         """ Returns iterable of policies across all policies in configs """
         def policy_data(data_):
             return data_.get("policies")
-        return map(policy_data, filter(policy_data, self.get_config_data()))
+        return itertools.chain(map(policy_data, filter(policy_data, self.get_config_data())))
 
     def get_policy_resource(self) -> Optional[str]:
         """
