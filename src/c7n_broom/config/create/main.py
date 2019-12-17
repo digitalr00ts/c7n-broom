8be3b28427e3250fa0e068c0a29be079f7b18d0b
Carlos Meza <carlos@digitalr00ts.com>
2019-12-16 16:43:04 -0800

    feat: added ability to pass c7n config options

--- src/c7n_broom/config/create/main.py
+++ src/c7n_broom/config/create/main.py
@@ -4,6 +4,8 @@ import logging
 from typing import Any, Dict, Optional, Union
 
 import boto_remora.aws
+import botocore
+from boto_remora.aws import Sts
 from vyper import Vyper
 
 from c7n_broom.config.create.policies import get_policy_files
@@ -27,13 +29,19 @@ def account_c7nconfigs(
     )
     # TODO: remove skip regions in favor of setting regions in broom config
     regions = boto_remora.aws.Ec2(name).available_regions if not skip_regions else list()
+
+    c7nconfig_kwargs = {
+        "profile": name,
+        "regions": regions,
+        "metrics_enabled": False,
+    }
+    c7nconfig_kwargs.update(global_settings.get("c7n", dict()))
+    c7nconfig_kwargs.update(account_settings.get("c7n", dict()))
+
     _LOGGER.debug("Creating policies: %s %s", name, policies)
     # TODO: remove hardcoded disabling metrics and move to broom config
     return map(
-        lambda policy_name: C7nConfig(
-            name, configs=(policy_name,), regions=regions, metrics_enabled=False,
-        ),
-        policies,
+        lambda policy_name: C7nConfig(configs=(policy_name,), **c7nconfig_kwargs), policies,
     )
 
 
@@ -45,6 +53,8 @@ def c7nconfigs(
     """ Create c7n configs for every policy and account """
     global_settings = config.get("global")
     accounts = config.get("accounts")
+    available_profiles = botocore.session.Session().available_profiles
+    accountids = {profile_: None for profile_ in available_profiles}
 
     if (
         accounts is not None
@@ -52,26 +62,44 @@ def c7nconfigs(
         and isinstance(accounts.get("ALL"), bool)
         and accounts.get("ALL") is True
     ):
-        import botocore  # pylint: disable=import-outside-toplevel
 
         _LOGGER.info("Loading all available profiles.")
-        accounts = {profile: None for profile in botocore.session.Session().available_profiles}
+        accounts = {profile: None for profile in available_profiles}
 
     if not skip_auth_check and accounts:
-        authed_profiles = boto_remora.aws.helper.get_authed_profiles(accounts.keys())
-        unauthed_profiles = set(accounts).difference(authed_profiles)
+        aws_stss = tuple(map(Sts, available_profiles))
+        accountids = {sts_.profile_name: sts_.account for sts_ in aws_stss}
+        authed_profiles = tuple(filter(lambda aid_: aid_[1], accountids.items()))
+        unauthed_profiles = set(available_profiles).difference(authed_profiles)
         msg = f"Not all accounts can access the AWS API {unauthed_profiles}."
         if skip_unauthed:
             if unauthed_profiles:
                 _LOGGER.info(msg)
-            accounts = dict(
-                filter(lambda account_: account_[0] in authed_profiles, accounts.items())
-            )
+                for unauthed_ in unauthed_profiles:
+                    _ = accounts.pop(unauthed_)
         elif unauthed_profiles:
             raise RuntimeError(msg)
 
+        ###
+        # authed_profiles = boto_remora.aws.helper.get_authed_profiles(accounts.keys())
+        # unauthed_profiles = set(accounts).difference(authed_profiles)
+        # msg = f"Not all accounts can access the AWS API {unauthed_profiles}."
+        # if skip_unauthed:
+        #     if unauthed_profiles:
+        #         _LOGGER.info(msg)
+        #     accounts = dict(
+        #         filter(lambda account_: account_[0] in authed_profiles, accounts.items())
+        #     )
+        # elif unauthed_profiles:
+        #     raise RuntimeError(msg)
+
     if not accounts:
         _LOGGER.critical("No accounts to create c7n configs.")
+
+    for profile_ in accounts:
+        data_ = accounts[profile_]
+        data_["c7n"] = data_.get("c7n", dict())
+        data_["c7n"]["account_id"] = data_["c7n"].get("account_id", accountids.get(profile_))
 
     return itertools.chain.from_iterable(
         map(
