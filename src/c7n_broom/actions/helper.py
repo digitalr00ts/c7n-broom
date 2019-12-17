88b57b1750da20886aef1fae4332de8eaf3348f0
Carlos Meza <carlos@digitalr00ts.com>
2019-12-17 14:04:24 -0800

    changed file names to not use spaces

--- src/c7n_broom/actions/helper.py
+++ src/c7n_broom/actions/helper.py
@@ -7,7 +7,7 @@ def account_profile_policy_str(c7n_config):
     Returns a string for the c7n_config.
     Used for creating files names.
     """
-    profile_policies_str = " - ".join(
+    profile_policies_str = ":".join(
         [
             c7n_config.profile,
             ", ".join(map(lambda policy: Path(policy).stem, c7n_config.configs)),
