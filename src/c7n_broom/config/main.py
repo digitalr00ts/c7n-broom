86ed0d50bd3ce16c25b1f609316094f40ef7dd90
Carlos Meza <carlos@digitalr00ts.com>
2020-01-24 14:02:30 -0800

    fix(config): cast sequences to list when casting c7ncfg

--- src/c7n_broom/config/main.py
+++ src/c7n_broom/config/main.py
@@ -4,6 +4,7 @@ import itertools
 import logging
 import os
 import pathlib
+from collections import abc
 from os import PathLike
 from pathlib import Path
 from typing import Any, Dict, Iterable, List, Optional
@@ -144,5 +145,8 @@ class C7nCfg:  # pylint: disable=too-many-instance-attributes
         """ Cast to c7n Config object """
         rtn = c7n.config.Config().empty()
         for key_, val_ in dataclasses.asdict(self).items():
+            if isinstance(val_, abc.Sequence):
+                _LOGGER.debug("Cast to list, %s", val_)
+                val_ = list(val_)
             setattr(rtn, key_, val_)
         return rtn
